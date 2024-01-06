d004d4c37d815c95749d20c66a755fe9c934e267
Najmul shaon <najmul.nh.shaon@gmail.com>
2024-01-07 00:22:16 +0600

    added spninner by daisy ui

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,16 +1,29 @@
-import { Link, Outlet } from "react-router-dom";
+import { NavLink, Outlet, useNavigation } from "react-router-dom";
+import "./Header.css";
+import Footer from "../Footer/Footer";
 
 const Header = () => {
+  const navigation = useNavigation();
   return (
     <nav>
       <div>
         <div className="flex gap-4 justify-center mb-8">
-          <Link to={"/"}>Home</Link>
-          <Link to={"/contact"}>Contact Us</Link>
-          <Link to={"/about"}>About</Link>
-          <Link to={"/posts"}>Posts</Link>
+          {/* <Link to={"/"}>Home</Link> */}
+          {/* <Link to={"/contact"}>Contact Us</Link> */}
+          {/* <Link to={"/about"}>About</Link> */}
+          {/* <Link to={"/posts"}>Posts</Link> */}
+          <NavLink to={"/"}>Home</NavLink>
+          <NavLink to={"/contact"}>Contact us</NavLink>
+          <NavLink to={"/about"}>About</NavLink>
+          <NavLink to={"/posts"}>Posts</NavLink>
         </div>
-        <Outlet></Outlet>
+        {navigation.state === "loading" ? (
+          <span className="loading loading-bars loading-lg"></span>
+        ) : (
+          <Outlet></Outlet>
+        )}
+
+        <Footer></Footer>
       </div>
     </nav>
   );
